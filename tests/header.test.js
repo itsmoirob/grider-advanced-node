182450ee2ef69dc2c11265d2136db1c42a99aef6
Robbie <rob.w.sherman@gmail.com>
2018-07-03 07:02:24 +0100

    its been a while

---
+++ tests/header.test.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+
+let browser;
+let page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({ headless: false });
+
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+test('The header has the correct text', async () => {
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+  expect(text).toEqual('Blogster');
+})
+
+test('clicking login start oauth flow', async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
+}) 
